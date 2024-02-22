10b64ca71d90abd74b18e4f5353c1550b1f56feb
Jonas Şahin <60566822+jonassahin01@users.noreply.github.com>
2024-02-22 20:34:10 +0200

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,26 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Jonas",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    role: "Trader",
+    bio: "this is my bio.",
+    email: "jonas.smsg@gmail.com",
+    github: "jonassahin01",
+    instagram: "jonas.sahin01",
+    x: "@jonassahin01",
+    youtube: "@Jonas066",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `jonas blog`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Jonas Blog",
+    description: "welcome to blog!",
   },
 
   // CONFIG configration (required)
@@ -53,7 +54,7 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
       "issue-term": "og:title",
