f6573d69888c56b156b59d1642de31545587419b
Jonas Şahin <60566822+jonassahin01@users.noreply.github.com>
2024-02-23 10:37:25 +0200

    Update site.config.js
    changed avatar

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Jonas",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Trader",
     bio: "this is my bio.",
     email: "jonas.smsg@gmail.com",
